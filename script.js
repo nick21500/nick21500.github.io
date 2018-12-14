4700c173ab4d4a7f0ebb156a7f73857f188b56ea
nick21500 <nick21500@yahoo.com>
2018-12-13 23:40:07 -0600

    Major update 1

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 /* jslint browser: true, devel: true */
 /* eslint-disable no-unused-vars, no-console */
-/* global $ */
+/* global $, alert, window */
 
 $(function () {
     "use strict";
 
-
+    var threshold = $("#section-1").offset().top;
+    
+    $(window).scroll(function(){
+        if (window.pageYOffset > threshold) {
+            $(".header").addClass("header-bg");
+        } else {
+            $(".header").removeClass("header-bg");
+        }
+    });
 
 });
