b50cd61f398f48ac1ba0afb46f9946be41f14282
nick21500 <nick21500@yahoo.com>
2020-04-04 21:59:03 -0500

    neon-clock

---
+++ projects/neon-clock/js/main.js
@@ -0,0 +1,35 @@
+/* jslint browser: true, devel: true */
+/* eslint-disable no-unused-vars, no-console */
+/* global $ */
+
+$(function () {
+    "use strict";
+
+    function loadImage(x) {
+        var preload = new createjs.LoadQueue();
+        preload.addEventListener("fileload", handleFileComplete);
+        preload.loadFile("../images/" + x + ".png");
+    }
+
+    window.setInterval(function () {
+        var now = new Date();
+
+        var hours = now.getHours();
+        if (hours >= 12) {
+            hours = hours - 12;
+        }
+        hours = ('0' + hours).slice(-2)
+
+        var minutes = now.getMinutes();
+
+        var time = hours + "" + minutes
+
+        //$("#time").text(time);
+
+        $("html").css("background-image", "url(../images/" + time + ".png)")
+        
+        loadImage(time + 1);
+
+    }, 1000);
+
+});
